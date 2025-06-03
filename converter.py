41af3b8477e39f90d694cb50c598c5bdfbd082a6
krystiandsw <mailbox08743@gmail.com>
2025-06-03 20:21:55 +0200

    Task3: Add JSON writing function

--- converter.py
+++ converter.py
@@ -60,3 +60,7 @@ def read_json(file_path):
             return json.load(f)
     except Exception as e:
         raise ValueError(f"Invalid JSON file: {e}")
+
+def write_json(data, file_path):
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
